e2803386884f1d3ebde8148a53744a98e0fbad43
Gareth Slinn <gareth.slinn@dugout.com>
2017-11-03 20:51:06 +0000

    apply fetch api

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,6 +1,12 @@
 <template>
     <div id="app">
         <h3>{{ title }}</h3>
+        <button
+                class="btn btn-primary"
+                @click="fetchCharacter"
+        >
+            Fetch character
+        </button>
     </div>
 </template>
 
@@ -10,6 +16,18 @@
         data() {
             return {
                 title: 'Generate your team'
+            }
+        },
+        methods: {
+            fetchCharacter() {
+                fetch(
+                    'https://swapi.co/api/people/', {
+                        method: 'GET'
+                    }
+                )
+                    .then(response => response.json())
+                    .then(json => console.log(json))
+
             }
         }
     }
