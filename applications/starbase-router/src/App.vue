57737259cb4b3d671822bb09777cd05fed6c9f4c
Gareth Slinn <gareth.slinn@dugout.com>
2017-11-03 22:36:48 +0000

    apply routes

--- applications/starbase-router/src/App.vue
+++ applications/starbase-router/src/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<div id="app">
 		<h3>Starbase Router</h3>
+		<h4>
+			<router-link to="/data/people">Meet the character</router-link> | 
+			<router-link to="/data/planets">Explore planets</router-link>
+		</h4>
 		<router-view></router-view>
 	</div>
 </template>
