39c2e660f7f036f2c4771eef2ef6589dab9e4598
Gareth Slinn <gareth.slinn@dugout.com>
2017-11-03 21:42:28 +0000

    complete starbase app

--- applications/starbase/src/App.vue
+++ applications/starbase/src/App.vue
@@ -1,34 +1,30 @@
 <template>
     <div id="app">
         <h3>{{ title }}</h3>
-        <button
-                class="btn btn-primary"
-                @click="fetchCharacter"
-        >
-            Fetch character
-        </button>
+        <div class="col-md-12">
+            <Character 
+                v-for="(id, index) in initial_ids"
+                :id="id" 
+                key="index"
+            />
+        </div>
+        
     </div>
 </template>
 
 <script>
+import Character from './components/Character.vue'
+
     export default {
         name: 'app',
         data() {
             return {
-                title: 'Generate your team'
+                title: 'Generate your team',
+                initial_ids: [1,13,14]
             }
         },
-        methods: {
-            fetchCharacter() {
-                fetch(
-                    'https://swapi.co/api/people/', {
-                        method: 'GET'
-                    }
-                )
-                    .then(response => response.json())
-                    .then(json => console.log(json))
-
-            }
+        components: {
+            Character
         }
     }
 </script>
