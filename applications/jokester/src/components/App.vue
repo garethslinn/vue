29c1427f1aaa7b22b1b5efc6a41c51d134e642ce
Gareth Slinn <gareth.slinn@dugout.com>
2017-11-06 11:29:58 +0000

    add jokes

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -1,7 +1,30 @@
 <template>
     <div id="app">
-        Got jokes?
-        {{ $store.state }}
+
+        <h3>Got jokes?</h3>
+        <button class="btn btn-primary" @click="initJokes">Add ten jokes</button>
+        <br><br>
+        <div class="col-md-12">
+            <Joke
+                v-for="(joke, index) in $store.state.jokes"
+                :joke="joke"
+                key="index"
+            />
+        </div>
+
     </div>
 </template>
 
+<script>
+    import { mapActions } from 'vuex'
+    import Joke from './Joke.vue'
+
+    export default {
+        methods: mapActions([
+            'initJokes'
+        ]),
+        components: {
+            Joke
+        }
+    }
+</script>
